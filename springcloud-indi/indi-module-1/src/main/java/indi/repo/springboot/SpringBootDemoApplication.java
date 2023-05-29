c232810e906bcaa048b3d7caab280fb765213f74
ChenHQ <472972984@qq.com>
2023-05-29 13:32:33 +0800

    feat：1）注入自定义配置信息config类

--- springcloud-indi/indi-module-1/src/main/java/indi/repo/springboot/SpringBootDemoApplication.java
+++ springcloud-indi/indi-module-1/src/main/java/indi/repo/springboot/SpringBootDemoApplication.java
@@ -2,9 +2,11 @@ package indi.repo.springboot;
 
 
 import com.thebeastshop.forest.springboot.annotation.ForestScan;
+import indi.repo.springboot.controller.DemoProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -21,6 +23,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @MapperScan("indi.repo.springboot.mapper")
 @ForestScan("indi.repo.springboot.forest")
+@EnableConfigurationProperties({
+        DemoProperties.class
+})
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
---
+++ springcloud-indi/indi-module-1/src/main/java/indi/repo/springboot/controller/DemoProperties.java
@@ -0,0 +1,22 @@
+package indi.repo.springboot.controller;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author ChenHQ
+ * @date 2023/5/24 19:20
+ */
+@ConfigurationProperties(prefix = "server")
+@Data
+public class DemoProperties {
+
+    @Data
+    public static class ChqClass {
+        String port;
+    }
+
+    ChqClass work = new ChqClass();
+
+
+}
