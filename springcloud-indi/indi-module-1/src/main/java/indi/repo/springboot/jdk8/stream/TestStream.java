e5f972e09d2701d349fa32ab6572e8c35d78fa62
ChenHQ <huiqi.chen@dbappsecurity.com.cn>
2023-08-09 15:19:22 +0800

    feat：1）流操作：分组取出指定字段

--- springcloud-indi/indi-module-1/src/main/java/indi/repo/springboot/jdk8/stream/TestStream.java
+++ springcloud-indi/indi-module-1/src/main/java/indi/repo/springboot/jdk8/stream/TestStream.java
@@ -1,9 +1,12 @@
 package indi.repo.springboot.jdk8.stream;
 
+import cn.hutool.core.date.DateUtil;
 import indi.repo.springboot.entity.Student;
+import indi.repo.springboot.entity.SystemLog;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,23 +20,46 @@ import java.util.stream.Stream;
 public class TestStream {
 
     private static List<Student> students = new ArrayList<>();
+    private static List<SystemLog> systemLogs = new ArrayList<>();
 
     static {
-        students.add(new Student(1L,"chq","男"));
-        students.add(new Student(2L,"zs","男"));
-        students.add(new Student(3L,"ssx","女"));
-        students.add(new Student(4L,"lb","男"));
-        students.add(new Student(5L,"wzj","女"));
-        students.add(new Student(6L,"gy","男"));
-        students.add(new Student(7L,"hz","男"));
-        students.add(new Student(8L,"dc","女"));
-        students.add(new Student(8L,"dc2","女"));
+        students.add(new Student(1L, "chq", "男"));
+        students.add(new Student(2L, "zs", "男"));
+        students.add(new Student(3L, "ssx", "女"));
+        students.add(new Student(4L, "lb", "男"));
+        students.add(new Student(5L, "wzj", "女"));
+        students.add(new Student(6L, "gy", "男"));
+        students.add(new Student(7L, "hz", "男"));
+        students.add(new Student(8L, "dc", "女"));
+        students.add(new Student(1L, "dc2", "女"));
+
+        //------------------------------------------------------
+
+        systemLogs.add(new SystemLog(1L, DateUtil.parseDate("2023-04-03 00:00:00")));
+        systemLogs.add(new SystemLog(2L, DateUtil.parseDate("2023-04-02 00:00:00")));
+        systemLogs.add(new SystemLog(3L, DateUtil.parseDate("2023-04-01 00:00:00")));
+        systemLogs.add(new SystemLog(4L, DateUtil.parseDate("2023-04-05 00:00:00")));
 
     }
 
 
     public static void main(String[] args) {
-        maxId();
+
+        System.out.println(students.stream().map(Student::getUsername).collect(Collectors.joining(",")));
+        String msg = "chq,zs,ssx,lb,wzj,gy,hz,dc,dc2,";
+        System.out.println(msg.substring(0, msg.length() - 1));
+
+        test4();
+
+    }
+
+    /**
+     * 分组取出指定字段
+     */
+    public static void test4() {
+        Map<Long, List<String>> map = students.stream()
+                .collect(Collectors.groupingBy(Student::getId, Collectors.mapping(Student::getUsername, Collectors.toList())));
+        System.out.println("map = " + map);
     }
 
     /**
@@ -64,6 +90,7 @@ public class TestStream {
 
     /**
      * 输出
+     *
      * @param students
      */
     public static void print(List<Student> students) {
@@ -81,7 +108,7 @@ public class TestStream {
         System.out.println(students.stream().count() == students.size());
 
         List<Student> list = new ArrayList<>();
-        list.add(new Student(10L,"test","男"));
+        list.add(new Student(10L, "test", "男"));
 
         System.out.println(Stream.concat(list.stream(), students.stream()).count());
 
