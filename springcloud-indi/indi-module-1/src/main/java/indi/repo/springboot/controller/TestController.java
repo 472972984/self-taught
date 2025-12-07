84489e22dae83692618a65dc48afc90c5fac877d
ChenHQ <huiqi.chen@dbappsecurity.com.cn>
2025-12-07 10:25:23 +0800

    feat：1）transfer bean

--- springcloud-indi/indi-module-1/src/main/java/indi/repo/springboot/controller/TestController.java
+++ springcloud-indi/indi-module-1/src/main/java/indi/repo/springboot/controller/TestController.java
@@ -1,37 +1,26 @@
 package indi.repo.springboot.controller;
 
-import com.alibaba.excel.EasyExcel;
-import indi.repo.common.annotation.RepeatSubmit;
-import indi.repo.common.exception.BaseException;
-import indi.repo.common.exception.enums.DemoExcepEnum;
-import indi.repo.common.module.Result;
-import indi.repo.common.utils.HibernateValidatorUtils;
-import indi.repo.springboot.common.cache.MemoryCache;
-import indi.repo.springboot.core.context.HandleContext;
-import indi.repo.springboot.core.context.LocalHandleContext;
-import indi.repo.springboot.entity.Student;
-import indi.repo.springboot.feign.api.TestApi;
-import indi.repo.springboot.forest.AgentClient;
-import indi.repo.springboot.log.InternalOpType;
-import indi.repo.springboot.log.annotation.OpLog;
-import indi.repo.springboot.log.core.OPLogContext;
-import indi.repo.springboot.mapper.StudentDao;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import indi.repo.springboot.module.dto.StudentDTO;
-import indi.repo.springboot.service.StudentService;
+import indi.repo.springboot.toexcel.DynamicExcelExporter;
+import indi.repo.springboot.toexcel.mt.MtRecommendResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.integration.redis.util.RedisLockRegistry;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -41,212 +30,97 @@ import java.util.List;
  * @date: 2021/6/18
  */
 @RestController
-@RequestMapping("test")
+@RequestMapping("")
 @RefreshScope
 @Slf4j
 public class TestController {
 
-    @Resource
-    private RedisLockRegistry lockRegistry;
+    public static void main(String[] args) throws JsonProcessingException {
 
-    @Value("${swagger2.enable:false}")
-    private String enable;
+        // /v1/poi/sputag/products
+        JSON json = JSONUtil.readJSON(new File("/Users/admin/Downloads/a.json"), Charset.defaultCharset());
+        MtRecommendResponse bean = json.toBean(MtRecommendResponse.class);
+         System.out.println("bean = " + bean);
+        MtRecommendResponse.Product product = bean.getSpus().get(0);
+        System.out.println(mapper.writeValueAsString(product));
 
-    @Autowired
-    private StudentService studentService;
-
-    @Autowired
-    private RedisTemplate redisTemplate;
-
-    @Autowired
-    private StudentDao studentDao;
-
-//    @Autowired
-    private TestApi testApi;
-
-    @Resource
-    private AgentClient client;
-
-    /**
-     * 测试项目启动web访问
-     *
-     * @return
-     */
-    @GetMapping("/hello")
-    public String testHello(String name) {
-//        System.out.println("chenhuiqi");
-        System.out.println("name = " + name);
-        System.out.println("enable = " + enable);
-        System.out.println(lockRegistry);
-
-        Student student = studentDao.selectByIdCustom(14L);
-        System.out.println("student = " + student);
-
-        return "world";
-    }
-
-    @GetMapping("/client")
-    public void testClient() {
-        String task = client.executeTask();
-        System.out.println("task = " + task);
-    }
-
-
-    @GetMapping("getStudentById/{Id}")
-    public Student getStudentById(@PathVariable("Id") String Id) {
-        Student student = new Student();
-        student.setId(Long.parseLong(Id));
-        student.setUsername("chenhuiqi");
-        return student;
-    }
-
-
-    /**
-     * 测试上传文件
-     *
-     * @param file
-     * @param name
-     * @return
-     */
-    @PostMapping("/file")
-    public String testFile(MultipartFile file, String name) {
-        System.out.println("name = " + name);
-        return name;
-    }
-
-
-    /**
-     * 整合mybatis-plus 测试访问
-     *
-     * @param id
-     * @return
-     */
-    @GetMapping("/student/{id}")
-    @OpLog(message = "获取学生信息", messageSuffix = "学生ID: {id}")
-    public Result<String> test(@PathVariable Long id) {
-        OPLogContext.putOpType(InternalOpType.OTHER);
-        Student student = studentService.queryStudent(id);
-        System.out.println("student = " + student);
-        OPLogContext.put("id", id);
-        return Result.ok("world");
-    }
-
-    /**
-     * 整合mybatis-plus 测试访问
-     *
-     * @return
-     */
-    @GetMapping("/student/getAll")
-    public Result<List<Student>> test22() {
-        return Result.ok((List<Student>) MemoryCache.getCache("studentAll", new ArrayList<>(), o -> {
-            log.info("hit from db");
-            return studentDao.selectAll();
-        }));
-    }
-
-    /**
-     * 全局异常捕获
-     *
-     * @return
-     */
-    @GetMapping("/throwException")
-    public String exception() {
-        System.out.println(redisTemplate);
-        throw new RuntimeException("异常信息");
-    }
-
-    /**
-     * 全局异常捕获
-     *
-     * @return
-     */
-    @GetMapping("/throwException2")
-    public String exception2() {
-        throw new BaseException(DemoExcepEnum.ERROR_TEST);
-    }
-
-
-    @GetMapping("/context")
-    public String context() {
-        HandleContext context = LocalHandleContext.getHandleContext();
-        System.out.println(context.getTraceId());
-        System.out.println(context.getDate());
-        return "context";
-    }
-
-    @PostMapping("/repeat")
-    @RepeatSubmit
-    public Result<String> repeat(@RequestBody Student student) {
-        System.out.println("student = " + student);
-        return Result.ok();
-    }
-
-    @GetMapping("/testAsync")
-    public String testAsync() {
-        HandleContext context = LocalHandleContext.getHandleContext();
-        System.out.println(context.getTraceId());
-        System.out.println(context.getDate());
-        studentService.testAsync(context.getUserId(), context.getTraceId());
-        return "context";
-    }
-
-    @GetMapping("/testValid")
-    public String testValid(@Validated StudentDTO studentDTO) {
-        System.out.println("studentDTO = " + studentDTO);
-        return "context";
-    }
-
-    @PostMapping("/testValidList")
-    public String testValidList(@RequestBody @Validated List<StudentDTO> studentDTOS) {
-        System.out.println("studentDTOS = " + studentDTOS);
-
-        studentDTOS.forEach(HibernateValidatorUtils::fastFailValidate);
-        return "context";
-    }
-
-    @GetMapping("/test/api")
-    public String testApi() {
-        String test = testApi.test();
-        System.out.println("test = " + test);
-        return "context";
-    }
-
-    @PostMapping("/upload")
-    public void upload(MultipartFile studentExcel) {
-//        EasyExcel.
-    }
-
-    @PostMapping("/read")
-    public void read() throws Exception {
-        File file = new File("C:\\Users\\Thompson\\Desktop\\导入Excel.xlsx");
-        List<StudentDTO> studentList = EasyExcel.read(file, StudentDTO.class, null).sheet().head(StudentDTO.class).doReadSync();
-        System.out.println(studentList);
-
-        List<StudentDTO> objects = EasyExcel.read(new FileInputStream(file)).sheet(0).head(StudentDTO.class).doReadSync();
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws Exception {
-
+//        File file = new File("/Users/admin/Downloads/1122.xlsx");
+//        MtRecommendExcelResponse bean = json.toBean(MtRecommendExcelResponse.class);
+//        DynamicExcelExporter.exportComplexData(bean.getSpus(), file);
 
     }
 
-    @GetMapping("/test/aop")
-    public void testAop() {
-        studentService.updateById(StudentDTO.builder().id(1L).username("chen").sex("man").build());
+    private static boolean validToken() {
+        Long expireTime = 1763110525L;
+        if (expireTime.toString().length() == 13) {
+            expireTime = expireTime / 1000;
+        }
+        return expireTime > System.currentTimeMillis() / 1000L;
     }
 
-    @GetMapping("/test/transactional")
-    public void transactional() {
-        studentService.testTransactionalA();
-        //studentService.testTransactionalB();
+    private static final ObjectMapper mapper;
+
+    static {
+        mapper = new ObjectMapper();
+        // 序列化配置
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        // 反序列化配置
+        // mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     }
 
-    @GetMapping("/testSpel")
-    public void testSpel() {
-        StudentDTO studentDTO = StudentDTO.builder().id(2L).sex("男").username("chenhuiqi").build();
-        String time = "2023-05-18";
-        studentService.testSpel(studentDTO, time);
+    @GetMapping("/toExcel")
+    public String toExcel(HttpServletResponse response) throws IOException {
+        List<StudentDTO> list = new ArrayList<>();
+
+        StudentDTO studentDTO = new StudentDTO();
+        studentDTO.setUsername("chq1");
+        studentDTO.setSex("M");
+        studentDTO.setId(0L);
+        studentDTO.setAge(Arrays.asList(1,2,3));
+        List<StudentDTO.Sport> sports = new ArrayList<>();
+        StudentDTO.Sport sport = new StudentDTO.Sport();
+        sport.setSize("100");
+        sport.setBall("basketball");
+        StudentDTO.Sport sport1 = new StudentDTO.Sport();
+        sport1.setSize("80");
+        sport1.setBall("football");
+
+        sports.add(sport);
+        sports.add(sport1);
+
+        studentDTO.setSports(sports);
+
+
+        StudentDTO studentDTO2 = new StudentDTO();
+        studentDTO2.setUsername("chq2");
+        studentDTO2.setSex("F");
+        studentDTO2.setId(1L);
+        studentDTO2.setAge(Arrays.asList(1,2,3));
+        List<StudentDTO.Sport> sports2 = new ArrayList<>();
+        StudentDTO.Sport sport3 = new StudentDTO.Sport();
+        sport3.setSize("100");
+        sport3.setBall("basketball");
+        StudentDTO.Sport sport4 = new StudentDTO.Sport();
+        sport4.setSize("80");
+        sport4.setBall("football");
+
+        sports2.add(sport3);
+        sports2.add(sport4);
+
+        studentDTO2.setSports(sports2);
+
+        list.add(studentDTO);
+        list.add(studentDTO2);
+
+        // FileExportUtils.downLoadReport(response, list, "11.xlsx", StudentDTO.class);
+
+        DynamicExcelExporter.exportComplexData(list, response, "复杂用户数据");
+
+        return "success";
     }
 
 
